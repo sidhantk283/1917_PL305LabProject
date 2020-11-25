28daacbec923b0a9374b54be473c284ab12a7dd0
Sidhant Karbotkar <mca.1917@unigoa.ac.in>
2020-11-25 22:52:33 +0530

    25Nov

---
+++ js/main.js
@@ -0,0 +1,72 @@
+$('.slider-one')
+.not('.slick-intialized')
+.slick({
+	autoplay:true,
+	autoplaySpeed:3000,
+	dots:true,
+	prevArrow:'.site-slider.slider-btn.prev',
+	nextArrow:'.site-slider.slider-btn.next',
+});
+
+$('.product-slider-4').slick({
+        autoplay: true,
+        infinite: true,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+
+
+    $('.product-slider-3').slick({
+        autoplay: true,
+        infinite: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
